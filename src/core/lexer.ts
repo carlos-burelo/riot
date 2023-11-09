c9cc4db75eabf02fbf43ddc6e151007f26dcba7a
carlos-burelo <carlosfernandoburelo@gmail.com>
2023-11-09 11:09:07 -0600

    :bug: fix: update lexer

--- src/core/lexer.ts
+++ src/core/lexer.ts
@@ -1,109 +1,271 @@
-const RESERVED_WORDS = ['var', 'const', 'int', 'float', 'str', 'bool', 'char', 'none']
-
 class Token {
-  type: string
-  value: any
-
-  constructor (type: string, value: any) {
-    this.type = type
-    this.value = value
-  }
+  constructor(public type: string, public value: string) {}
 }
 
 class Lexer {
-  text: string
-  position: number
-  currentChar: string | null
+  private input: string;
+  private position: number;
+  private currentChar: string | null;
 
-  constructor (text: string) {
-    this.text = text
-    this.position = 0
-    this.currentChar = this.text[this.position]
+  constructor(input: string) {
+    this.input = input;
+    this.position = 0;
+    this.currentChar = input.charAt(0);
   }
 
-  advance () {
-    this.position += 1
-    if (this.position > this.text.length - 1) {
-      this.currentChar = null
-    } else {
-      this.currentChar = this.text[this.position]
-    }
+  private advance() {
+    this.position++;
+    this.currentChar = this.position < this.input.length ? this.input.charAt(this.position) : null;
   }
 
-  skipWhitespace () {
+  private skipWhitespace() {
     while (this.currentChar !== null && /\s/.test(this.currentChar)) {
-      this.advance()
+      this.advance();
     }
   }
 
-  skipComment () {
-    while (this.currentChar !== '\n') {
-      this.advance()
-    }
-    this.advance()
+  private isAlphaNumeric(char: string | null) {
+    return /[a-zA-Z0-9_]/.test(char || "");
   }
 
-  integer () {
-    let result = ''
-    while (this.currentChar !== null && /[0-9]/.test(this.currentChar)) {
-      result += this.currentChar
-      this.advance()
-    }
-    return parseInt(result)
+  private isDigit(char: string | null) {
+    return /\d/.test(char || "");
   }
 
-  string () {
-    let result = ''
-    const quoteType = this.currentChar
-    this.advance()
-    while (this.currentChar !== null && this.currentChar !== quoteType) {
-      result += this.currentChar
-      this.advance()
-    }
-    this.advance()
-    return result
-  }
-
-  identifier () {
-    let result = ''
-    while (this.currentChar !== null && /[a-zA-Z_]/.test(this.currentChar)) {
-      result += this.currentChar
-      this.advance()
-    }
-    if (RESERVED_WORDS.includes(result)) {
-      return new Token(result.toUpperCase(), result)
-    }
-    return new Token('IDENTIFIER', result)
-  }
-
-  getNextToken () {
+  private getNextToken(): Token | null {
     while (this.currentChar !== null) {
       if (/\s/.test(this.currentChar)) {
-        this.skipWhitespace()
-        continue
+        this.skipWhitespace();
+        continue;
       }
-      if (this.currentChar === '#') {
-        this.skipComment()
-        continue
+
+      if (this.isAlphaNumeric(this.currentChar)) {
+        let value = "";
+        while (this.isAlphaNumeric(this.currentChar)) {
+          value += this.currentChar;
+          this.advance();
+        }
+
+        // Check if the identifier is a keyword
+        const keywords = ["break", "case", "catch", "class", "const", "continue", "debugger", "default", "delete",
+          "do", "else", "enum", "export", "extends", "false", "finally", "for", "function", "if", "import",
+          "in", "instanceof", "new", "null", "return", "super", "switch", "this", "throw", "true", "try",
+          "typeof", "undefined", "var", "void", "while", "with"];
+        const type = keywords.includes(value) ? value.toUpperCase() : "IDENTIFIER";
+        return new Token(type, value);
       }
-      if (/\d/.test(this.currentChar)) {
-        return new Token('INTEGER', this.integer())
+
+      if (this.isDigit(this.currentChar) || (this.currentChar === "." && this.isDigit(this.input[this.position + 1]))) {
+        let number = "";
+        while (this.isDigit(this.currentChar) || this.currentChar === ".") {
+          number += this.currentChar;
+          this.advance();
+        }
+        return new Token("NUMBER", number);
       }
-      if (/'|"/.test(this.currentChar)) {
-        return new Token('STRING', this.string())
+
+      if (this.currentChar === ";") {
+        this.advance();
+        return new Token("SEMICOLON", ";");
       }
-      if (/[a-zA-Z_]/.test(this.currentChar)) {
-        return this.identifier()
+
+      if (this.currentChar === "{") {
+        this.advance();
+        return new Token("LEFT_BRACE", "{");
       }
-      if (/[+\-*/%=(),\[\]]/.test(this.currentChar)) {
-        const token = new Token(this.currentChar, this.currentChar)
-        this.advance()
-        return token
+
+      if (this.currentChar === "}") {
+        this.advance();
+        return new Token("RIGHT_BRACE", "}");
       }
-      throw new Error(`Invalid character: ${this.currentChar}`)
+
+      if (this.currentChar === "(") {
+        this.advance();
+        return new Token("LEFT_PAREN", "(");
+      }
+
+      if (this.currentChar === ")") {
+        this.advance();
+        return new Token("RIGHT_PAREN", ")");
+      }
+
+      if (this.currentChar === "[") {
+        this.advance();
+        return new Token("LEFT_BRACKET", "[");
+      }
+
+      if (this.currentChar === "]") {
+        this.advance();
+        return new Token("RIGHT_BRACKET", "]");
+      }
+
+      if (this.currentChar === ",") {
+        this.advance();
+        return new Token("COMMA", ",");
+      }
+
+      if (this.currentChar === ".") {
+        this.advance();
+        return new Token("DOT", ".");
+      }
+
+      if (this.currentChar === "+") {
+        this.advance();
+        return new Token("PLUS", "+");
+      }
+
+      if (this.currentChar === "-") {
+        this.advance();
+        return new Token("MINUS", "-");
+      }
+
+      if (this.currentChar === "*") {
+        this.advance();
+        return new Token("ASTERISK", "*");
+      }
+
+      if (this.currentChar === "/") {
+        this.advance();
+        return new Token("SLASH", "/");
+      }
+
+      if (this.currentChar === "%") {
+        this.advance();
+        return new Token("PERCENT", "%");
+      }
+
+      if (this.currentChar === "=") {
+        if (this.input[this.position + 1] === "=") {
+          this.advance();
+          this.advance();
+          return new Token("EQUAL_EQUAL", "==");
+        } else {
+          this.advance();
+          return new Token("EQUAL", "=");
+        }
+      }
+
+      if (this.currentChar === "!") {
+        if (this.input[this.position + 1] === "=") {
+          this.advance();
+          this.advance();
+          return new Token("NOT_EQUAL", "!=");
+        } else {
+          this.advance();
+          return new Token("NOT", "!");
+        }
+      }
+
+      if (this.currentChar === ">") {
+        if (this.input[this.position + 1] === "=") {
+          this.advance();
+          this.advance();
+          return new Token("GREATER_EQUAL", ">=");
+        } else {
+          this.advance();
+          return new Token("GREATER", ">");
+        }
+      }
+
+      if (this.currentChar === "<") {
+        if (this.input[this.position + 1] === "=") {
+          this.advance();
+          this.advance();
+          return new Token("LESS_EQUAL", "<=");
+        } else {
+          this.advance();
+          return new Token("LESS", "<");
+        }
+      }
+
+      if (this.currentChar === "&" && this.input[this.position + 1] === "&") {
+        this.advance();
+        this.advance();
+        return new Token("AND", "&&");
+      }
+
+      if (this.currentChar === "|" && this.input[this.position + 1] === "|") {
+        this.advance();
+        this.advance();
+        return new Token("OR", "||");
+      }
+
+      if (this.currentChar === "?") {
+        this.advance();
+        return new Token("QUESTION_MARK", "?");
+      }
+
+      if (this.currentChar === ":") {
+        this.advance();
+        return new Token("COLON", ":");
+      }
+
+      if (this.currentChar === "'") {
+        let stringLiteral = "";
+        this.advance();
+        while (this.currentChar !== "'" && this.currentChar !== null) {
+          stringLiteral += this.currentChar;
+          this.advance();
+        }
+        if (this.currentChar === "'") {
+          this.advance();
+          return new Token("STRING", stringLiteral);
+        } else {
+          throw new Error("Unterminated string literal");
+        }
+      }
+
+      if (this.currentChar === "\"") {
+        let stringLiteral = "";
+        this.advance();
+        while (this.currentChar !== "\"" && this.currentChar !== null) {
+          stringLiteral += this.currentChar;
+          this.advance();
+        }
+        if (this.currentChar === "\"") {
+          this.advance();
+          return new Token("STRING", stringLiteral);
+        } else {
+          throw new Error("Unterminated string literal");
+        }
+      }
+
+      throw new Error(`Invalid character: ${this.currentChar}`);
     }
-    return new Token('EOF', null)
+
+    // Handle end of input
+    return null;
+  }
+
+  public tokenize(): Token[] {
+    const tokens: Token[] = [];
+
+    let token = this.getNextToken();
+    while (token !== null) {
+      tokens.push(token);
+      token = this.getNextToken();
+    }
+
+    return tokens;
   }
 }
 
-export default Lexer
+// Example usage
+const code = `
+  function add(a: number, b: number): number {
+    return a + b;
+  }
+
+  let x = 42;
+  let y = "Hello, world!";
+  if (x > 0) {
+    console.log(y);
+  } else {
+    console.log("x is not positive");
+  }
+`;
+
+const lexer = new Lexer(code);
+const tokens = lexer.tokenize();
+
+console.log(tokens);
